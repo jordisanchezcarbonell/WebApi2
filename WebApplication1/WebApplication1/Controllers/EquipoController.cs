f656c006cde62ab9741d5ca8a7d8feef5a37a7b1
Oriol <oriol592@gmail.com>
2019-03-23 19:47:40 +0100

    He afegit el controlador per Equips i per Instalacions, i he provat que totes les comandes funcionaven.

---
+++ WebApplication1/WebApplication1/Controllers/EquipoController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApplication1;
+
+namespace WebApplication1.Controllers
+{
+    public class EquipoController : ApiController
+    {
+        private ProyectoBDJordiEntities db = new ProyectoBDJordiEntities();
+
+        // GET: api/Equipo
+        public IQueryable<Equipo> GetEquipo()
+        {
+            //Evito que em retorni tots els objectes enllaçats:
+            db.Configuration.LazyLoadingEnabled = false;
+
+            return db.Equipo;
+        }
+
+        // GET: api/Equipo/5
+        [ResponseType(typeof(Equipo))]
+        public IHttpActionResult GetEquipo(int id)
+        {
+            //Evito que em retorni tots els objectes enllaçats:
+            db.Configuration.LazyLoadingEnabled = false;
+
+            Equipo equipo = db.Equipo.Find(id);
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(equipo);
+        }
+
+        // PUT: api/Equipo/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutEquipo(int id, Equipo equipo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != equipo.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(equipo).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EquipoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Equipo
+        [ResponseType(typeof(Equipo))]
+        public IHttpActionResult PostEquipo(Equipo equipo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Equipo.Add(equipo);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = equipo.id }, equipo);
+        }
+
+        // DELETE: api/Equipo/5
+        [ResponseType(typeof(Equipo))]
+        public IHttpActionResult DeleteEquipo(int id)
+        {
+            Equipo equipo = db.Equipo.Find(id);
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+
+            db.Equipo.Remove(equipo);
+            db.SaveChanges();
+
+            return Ok(equipo);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EquipoExists(int id)
+        {
+            return db.Equipo.Count(e => e.id == id) > 0;
+        }
+    }
+}
